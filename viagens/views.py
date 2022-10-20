e1d635d445cf07bdb229308df4d1ce15b4846dea
phlaviosales <phlaviosales@gmail.com>
2022-10-19 21:08:22 -0300

    Senac Django Forms

---
+++ viagens/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from viagens.forms import ViagemForms
+
+def index(request):
+    form= ViagemForms()
+    contexto={'form': form}
+    return render(request, 'index.html', contexto)
+
+def revConsulta(request):
+    if request.method =='POST':
+        form= ViagemForms(request.POST)
+        contexto={'form': form}
+        return render(request, 'consulta.html', contexto)
